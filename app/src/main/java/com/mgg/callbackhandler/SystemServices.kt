4fa601fd3919fae03715cdedcb1049aa088bc06b
forevermgg <centforever@163.com>
2023-09-09 14:44:02 +0800

    1

--- app/src/main/java/com/mgg/callbackhandler/SystemServices.kt
+++
@@ -1,7 +0,0 @@
-package com.mgg.callbackhandler
-
-import android.content.pm.PackageManager
-
-object SystemServices {
-  val packageManager: PackageManager by lazy { LibCheckerApp.app.packageManager }
-}
